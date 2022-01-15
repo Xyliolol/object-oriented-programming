6408ded214e8447c513a77f1e23c3e16b15719c2
Deo <jeeytis@yandex.ru>
2022-01-15 18:48:44 +0300

    4 задание 2 урока

--- Lesson2/Lesson2/BankAccount.cs
+++ Lesson2/Lesson2/BankAccount.cs
@@ -13,27 +13,75 @@ namespace Lesson2
         Cumulative
     }
     class BankAccount
-    { 
-        private static int _Number = 1;
-        private static int Number { get { return _Number++; }  }
-               
-        private decimal Balance { get; set; }
-        public BankAccount( decimal balance)
+    {
+        private static int i = 1;
+        int calc()
         {
-            Balance = balance; 
-            
+            return i++;
+        }
+        private int _Number;
+        public int Number
+        {
+            get
+            {
+                return this._Number;
+            }
+            set
+            {
+                this._Number = value;
+
+            }
+
         }
 
-        private TypeAcc Type { get; set; }
+        private decimal _Balance;
+
+        public decimal Balance
+        {
+            get
+            {
+                return this._Balance;
+            }
+            set
+            {
+                this._Balance = value;
+            }
+        } 
+        TypeAcc _Type;
+        public TypeAcc Type
+        {
+            get
+            {
+                return this._Type;
+            }
+            set
+            {
+                this._Type = value;
+            }
+        }
+        public BankAccount(decimal balance)
+        {
+
+            Balance = balance;
+            _Number = calc();
+        }
+       
+        public BankAccount()
+        {
+            _Number = calc();
+
+        }
 
         public BankAccount(TypeAcc type)
         {
             Type = type;
+            _Number = calc();
         }
-        public BankAccount(decimal balance,TypeAcc type)
+        public BankAccount(decimal balance, TypeAcc type)
         {
             Balance = balance;
             Type = type;
+            _Number = calc();
         }
 
         public void OUT()
