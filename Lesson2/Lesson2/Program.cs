ad4e578c38092bf6a853698021d11cb6d8ed361f
Deo <jeeytis@yandex.ru>
2022-01-19 16:04:34 +0300

    урок 3 задание 2

--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -4,154 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson2
+namespace Lesson3
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            BankAccount bankAccount = new BankAccount( 554875);
-            BankAccount bankAccount1 = new BankAccount(TypeAcc.Credite);
-            BankAccount bankAccount2 = new BankAccount(554875, TypeAcc.Credite);
-
-            bankAccount.OUT();
-            bankAccount1.OUT();
-            bankAccount2.OUT();
-
-
-            List<BankAccount> NumberList = new();
-            NumberList.Add(bankAccount);
-            NumberList.Add(bankAccount1);
-            NumberList.Add(bankAccount2);
-
-            bool no_account = true;
-            bool _isTake = true;
-
-            Console.WriteLine("Введите номер счета");
-            int vvodAcc = int.Parse(Console.ReadLine());
-            decimal vvodSum = 0;
-            int vvodAccTo = 0;
-
-            switch (vvodAcc)
-            {
-                case 1:
-                    {
-                        Console.WriteLine("Выбран счет №1");
-                        Console.WriteLine("Выберете счёт с которого будут переведены деньги:");
-                        try
-                        {
-
-                            vvodAcc = int.Parse(Console.ReadLine());
-                            foreach (var item in NumberList)
-                            {
-                                if (item.Number == vvodAcc)
-                                {
-                                    no_account = false;
-                                    break;
-                                }
-                            }
-                        }
-                        catch (Exception)
-                        {
-
-                            Console.WriteLine("Не корректный номер счёта");
-                        }
-                        Console.WriteLine("Выберите сумму,которую хотите списать со счета");
-                        try
-                        {
-                            vvodSum = decimal.Parse(Console.ReadLine());
-                            _isTake = false;
-                        }
-                        catch (Exception)
-                        {
-
-                            Console.WriteLine("Нельзя списать такое колличество");
-                        }
-                         bankAccount.Transfer(NumberList[vvodAcc - 1], vvodSum);
-                        break;
-                    }
-                    break;
-                case 2:
-                    {
-                        Console.WriteLine("Выбран счет №2");
-                        Console.WriteLine("Выберете счёт с которого будут переведены деньги:");
-                        try
-                        {
-
-                            vvodAcc = int.Parse(Console.ReadLine());
-                            foreach (var item in NumberList)
-                            {
-                                if (item.Number == vvodAcc)
-                                {
-                                    no_account = false;
-                                    break;
-                                }
-                            }
-                        }
-                        catch (Exception)
-                        {
-
-                            Console.WriteLine("Не корректный номер счёта");
-                        }
-                        Console.WriteLine("Выберите сумму,которую хотите списать со счета");
-                        try
-                        {
-                            vvodSum = decimal.Parse(Console.ReadLine());
-                            _isTake = false;
-                        }
-                        catch (Exception)
-                        {
-
-                            Console.WriteLine("Нельзя списать такое колличество");
-                        }
-                        bankAccount1.Transfer(NumberList[vvodAcc - 1], vvodSum);
-                        break;
-                    }
-                    break;
-                case 3:
-                    {
-                        Console.WriteLine("Выбран счет №3");
-                        Console.WriteLine("Выберете счёт с которого будут переведены деньги:");
-                        try
-                        {
-
-                            vvodAcc = int.Parse(Console.ReadLine());
-                            foreach (var item in NumberList)
-                            {
-                                if (item.Number == vvodAcc)
-                                {
-                                    no_account = false;
-                                    break;
-                                }
-                            }
-                        }
-                        catch (Exception)
-                        {
-
-                            Console.WriteLine("Не корректный номер счёта");
-                        }
-                        Console.WriteLine("Выберите сумму,которую хотите списать со счета");
-                        try
-                        {
-                            vvodSum = decimal.Parse(Console.ReadLine());
-                            _isTake = false;
-                        }
-                        catch (Exception)
-                        {
-
-                            Console.WriteLine("Нельзя списать такое колличество");
-                        }
-                        bankAccount2.Transfer(NumberList[vvodAcc - 1], vvodSum);
-                        break;
-                    }
-                    break;
-
-                default:
-                    break;
-
-            }
-
+    { 
+        static string StringReverse(string vvod)
+        { 
+            char[] vvodReverse = vvod.ToCharArray();
+            Array.Reverse(vvodReverse);
+            return new string(vvodReverse);
+            
         }
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Введите любой текст");
+            string vvod = Console.ReadLine();
+            vvod = StringReverse(vvod);
+            Console.WriteLine(vvod);
+        }
+       
     }
-    
+   
 }
