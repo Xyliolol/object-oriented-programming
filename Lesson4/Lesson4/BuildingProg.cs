3730814128b3358b7a51d0ca3aa790e3327cbf82
Deo <jeeytis@yandex.ru>
2022-01-23 14:43:51 +0300

    урок 4 задание 1

---
+++ Lesson4/Lesson4/BuildingProg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class BuildingProg
+    {
+
+        private int BuidingNumber;
+        static int count = 1;
+        private int floor ;
+        private int flat ;
+        private int entrance ;
+        private float height ;
+        public void Building(int _floor, int _flat, int _entrance, float _height)
+        {
+            BuidingNumber = CountPlus(count);
+            height = _height;
+            floor = _floor;
+            flat = _flat;
+            entrance = _entrance;
+
+        }
+          
+        public void FloorHeight(float height,int floor)
+        {
+              BuidingNumber = count++;
+            Console.WriteLine($"номер дома {BuidingNumber}");
+            float floorheight = height / floor;
+            Console.WriteLine($"высота этажа составляет {floorheight} метра"); 
+        }
+        public void FlatInEntrance(int flat , int entrance)
+        {
+            int flatinentrance = flat / entrance;
+            Console.WriteLine($"количество квартир в подьезде :{flatinentrance}");
+        }
+        public void FlatInFloor(int flat, int floor)
+        {
+            int flatinfloor = flat / floor ;
+            Console.WriteLine($"квартир на лесничной площадке:{flatinfloor}");
+        }
+        public int CountPlus(int count)
+        {
+            return count++;
+        }
+       
+    }
+}
--- Lesson4/Lesson4/Program.cs
+++ Lesson4/Lesson4/Program.cs
@@ -6,7 +6,19 @@ namespace Lesson4
     {
         static void Main(string[] args)
         {
-            
+            var Building = new BuildingProg();
+           
+            Building.FloorHeight(70,20);
+            Building.FlatInEntrance(400,4);
+            Building.FlatInFloor(400,20);
+
+            var Building2 = new BuildingProg();
+
+            Building2.FloorHeight(80,30);
+            Building2.FlatInEntrance(300,5);
+            Building2.FlatInFloor(300,30);
         }
+       
     }
+
 }
