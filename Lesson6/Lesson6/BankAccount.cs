d3b4d16a526d15f6fe335bc3b93011c48d29f391
Deo <jeeytis@yandex.ru>
2022-01-28 22:10:35 +0300

    урок 6 задание 1

---
+++ Lesson6/Lesson6/BankAccount.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    enum TypeAcc
+    {
+        Credite,
+        Debet,
+        Cumulative
+    }
+    class BankAccount
+    {
+        private static int i = 1;
+        int calc()
+        {
+            return i++;
+        }
+        private int _Number;
+        public int Number
+        {
+            get
+            {
+                return this._Number;
+            }
+            set
+            {
+                this._Number = value;
+
+            }
+
+        }
+
+        private decimal _Balance;
+
+        public decimal Balance
+        {
+            get
+            {
+                return this._Balance;
+            }
+            set
+            {
+                this._Balance = value;
+            }
+        }
+        TypeAcc _Type;
+        public TypeAcc Type
+        {
+            get
+            {
+                return this._Type;
+            }
+            set
+            {
+                this._Type = value;
+            }
+        }
+        public BankAccount(decimal balance)
+        {
+
+            Balance = balance;
+            _Number = calc();
+        }
+
+        public BankAccount()
+        {
+            _Number = calc();
+
+        }
+
+        public BankAccount(TypeAcc type)
+        {
+            Type = type;
+            _Number = calc();
+        }
+        public BankAccount(decimal balance, TypeAcc type)
+        {
+            Balance = balance;
+            Type = type;
+            _Number = calc();
+        }
+
+        public void OUT()
+        {
+            Console.WriteLine($"номер счета {Number}\t баланс {Balance}\t тип счета {Type}\t");
+
+        }
+        public static bool operator == (BankAccount one, BankAccount two) { return one.Equals(two); }
+        public static bool operator != (BankAccount one, BankAccount two) { return !(one == two); }
+
+        public override string ToString()
+        {
+                string a = $"номер счета {Number}\t баланс {Balance}\t тип счета {Type}\t";
+                return a;           
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Number,Balance,Type);
+        }
+        public bool Equals(BankAccount other)
+        {
+            if (other is null)
+                return false;
+            else
+            {
+              return other.Number == Number && other.Balance == Balance && other.Type == Type;
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount(55000,TypeAcc.Credite);
+            BankAccount bankAccount1 = new BankAccount(100000,TypeAcc.Credite);
+            BankAccount bankAccount2 = new BankAccount(554000, TypeAcc.Credite);
+            BankAccount bankAccount3 = new BankAccount(554000, TypeAcc.Credite);
+
+            bankAccount.OUT();
+            bankAccount1.OUT();
+            bankAccount2.OUT();
+            Console.WriteLine("проверка на равенство");
+            Console.WriteLine(bankAccount1 == bankAccount1);
+            Console.WriteLine("проверка на равенство");
+            Console.WriteLine(bankAccount == bankAccount1);
+            Console.WriteLine("проверка на не равенство");
+            Console.WriteLine(bankAccount1 != bankAccount2);
+            Console.WriteLine("проверка на не равенство");
+            Console.WriteLine(bankAccount1 != bankAccount1);
+            Console.WriteLine("перевод в строковый тип");
+            Console.WriteLine(bankAccount.ToString());
+            Console.WriteLine("хэш код аккаунта");
+            Console.WriteLine(bankAccount.GetHashCode());
+            Console.WriteLine("переопределенный Equals");
+            Console.WriteLine(bankAccount2.Equals(bankAccount3));
+            Console.WriteLine("переопределенный Equals");
+            Console.WriteLine(bankAccount2.Equals(bankAccount2));
+        }
+    }
+}
+
