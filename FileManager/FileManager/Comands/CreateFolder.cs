4e4779498a248f87caf269e7a8c0d5e9de1538cc
Deo <jeeytis@yandex.ru>
2022-02-05 19:25:15 +0300

    урок 7 начало работы

---
+++ FileManager/FileManager/Comands/CreateFolder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Comands
+{
+    class CreateFolder : AllCommands
+    {
+        public CreateFolder()
+        {
+            Name = "cd";
+            Description = "создание дирректории";
+        }
+        public override void Execute()
+        {
+            var path = Directory.GetCurrentDirectory();          
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
---
+++ FileManager/FileManager/Comands/DriveInfoFileManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Comands
+{
+    class DriveInfoFileManager : AllCommands
+    {
+        public DriveInfoFileManager()
+        {
+            Name = "di";
+            Description = "свойства диска";
+        }
+        public override void Execute()
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                {
+                    try
+                    {
+                        Console.WriteLine("Имя диска: " + drive.Name);
+                        Console.WriteLine("Файловая система: " + drive.DriveFormat);
+                        Console.WriteLine("Тип диска: " + drive.DriveType);
+                        Console.WriteLine("Объем доступного свободного места (в байтах): " + drive.AvailableFreeSpace);
+                        Console.WriteLine("Готов ли диск: " + drive.IsReady);
+                        Console.WriteLine("Корневой каталог диска: " + drive.RootDirectory);
+                        Console.WriteLine("Общий объем свободного места, доступного на диске (в байтах): " + drive.TotalFreeSpace);
+                        Console.WriteLine("Размер диска (в байтах): " + drive.TotalSize);
+                        Console.WriteLine("Метка тома диска: " + drive.VolumeLabel);
+                    }
+                    catch { }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
